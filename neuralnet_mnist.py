cfb668a11302b915c4b315e842d4b4f0a389159f
naokifile <startri.sea@gmail.com>
2019-12-26 17:28:44 +0900

    20191226

---
+++ neuralnet_mnist.py
@@ -0,0 +1,24 @@
+import sys, os
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+import pickle
+from neural_network import sigmoid
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    
